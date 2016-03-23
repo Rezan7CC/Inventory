dd14dfc52fd1f05e18e3d905a5b081717c2fa8c5
Rezan <rezan@kizilguen.de>
2016-03-23 05:19:03 +0100

    Added SQLite 3

---
+++ Assets/Inventory/DatabaseTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Data;
+using Mono.Data.SqliteClient;
+
+public class DatabaseTest : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {  
+        SqliteConnection dbCon = new SqliteConnection("URI=file:test.db;");
+        dbCon.Open();
+        SqliteCommand selectAll = new SqliteCommand("select * from TestTable", dbCon);
+        SqliteDataReader selectAllReader = selectAll.ExecuteReader();
+        int bp = 1;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	
+	}
+}
