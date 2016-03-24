3ebe3618f9bea7d36346f6d99c0bfaead4536625
Rezan <rezan@kizilguen.de>
2016-03-24 21:44:51 +0100

    Implemented server send item methods

--- Assets/Inventory/Networking/NetworkManager.cs
+++ Assets/Inventory/Networking/NetworkManager.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using Items;
 
 namespace Networking
 {
     public struct NetworkMessageType
     {
         public static short AddItem = MsgType.Highest + 1;
+        public static short AddItems = MsgType.Highest + 2;
     }
 
     public class DataMessage : MessageBase
     {
         public object data;
+    }
+
+    public class DataArrayMessage : MessageBase
+    {
+        public object[] data;
+        public int arraySize;
     }
 
     public class NetworkManager : MonoBehaviour
--- Assets/Inventory/Networking/Server.cs
+++ Assets/Inventory/Networking/Server.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine.Networking;
 using Items;
 using Database;
+using System.Collections.Generic;
 
 namespace Networking
 {
@@ -8,6 +9,7 @@ namespace Networking
     {
         NetworkManager networkManager = null;
         DatabaseManager databaseManager = new DatabaseManager();
+        static int maxItemsPerPackage = 10;
 
         public override void Setup(NetworkManager networkManager)
         {
@@ -17,18 +19,66 @@ namespace Networking
             NetworkServer.Listen(networkManager.serverPort);
             NetworkServer.RegisterHandler(MsgType.Connect, OnClientConnected);
             NetworkServer.RegisterHandler(NetworkMessageType.AddItem, OnAddItem);
-           
         }
 
         void OnClientConnected(NetworkMessage clientConnectedMsg)
         {
-            
+            List<Weapon> weapons = DatabaseManager.GetWeapons();
+            SendItems(clientConnectedMsg.conn.connectionId, weapons.ToArray(), weapons.Count);
+
+            List<Armor> armors = DatabaseManager.GetArmors();
+            SendItems(clientConnectedMsg.conn.connectionId, armors.ToArray(), armors.Count);
+
+            List<Consumable> consumables = DatabaseManager.GetConsumable();
+            SendItems(clientConnectedMsg.conn.connectionId, consumables.ToArray(), consumables.Count);
         }
 
         void OnAddItem(NetworkMessage addItemMsg)
         {
             DataMessage itemMessage = addItemMsg.ReadMessage<DataMessage>();
             Item item = (Item)itemMessage.data;
+
+            RedirectItem(addItemMsg.conn.connectionId, item);
+        }
+
+        public void SendToAllExcept(int connectionId, short msgType, MessageBase message)
+        {
+            foreach(NetworkConnection connection in NetworkServer.connections)
+            {
+                if (connection.connectionId == connectionId)
+                    continue;
+
+                NetworkServer.SendToClient(connectionId, msgType, message);
+            }
+        }
+
+        public void RedirectItem(int sourceConnectionID, Item item)
+        {
+            DataMessage addItemMessage = new DataMessage();
+            addItemMessage.data = item;
+            SendToAllExcept(sourceConnectionID, NetworkMessageType.AddItem, addItemMessage);
+        }
+
+        public void SendItems(int connectionId, Item[] items, int arraySize)
+        {
+            int i = 0;
+            while(i < arraySize)
+            {
+                Item[] itemPackage = new Item[maxItemsPerPackage];
+
+                for (int ii = 0; ii < maxItemsPerPackage; ii++, i++)
+                {
+                    if (i >= arraySize)
+                        break;
+
+                    itemPackage[ii] = items[i];
+                }
+
+                DataArrayMessage dataArrayMsg = new DataArrayMessage();
+                dataArrayMsg.arraySize = maxItemsPerPackage;
+                dataArrayMsg.data = itemPackage;
+                NetworkServer.SendToClient(connectionId, NetworkMessageType.AddItems, dataArrayMsg);
+            }
         }
     }
 }
