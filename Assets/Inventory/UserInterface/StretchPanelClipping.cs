3e9541d8b455968b7cdd6f0ddd944876eb7970cf
Rezan <rezan@kizilguen.de>
2016-03-28 06:41:58 +0200

    Added scrollable user interface panel

---
+++ Assets/Inventory/UserInterface/StretchPanelClipping.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UserInterface
+{
+    [ExecuteInEditMode]
+    public class StretchPanelClipping : MonoBehaviour
+    {
+        public UIPanel panel;
+        public Camera uiCamera;
+        /// <summary> In seconds </summary>
+        public float updateInterval = 0.1f;
+        public Vector2 offset = new Vector2(0, 0);
+
+        Coroutine stretchCoroutine = null;
+
+        void Start()
+        {
+            if(stretchCoroutine == null)
+                stretchCoroutine = StartCoroutine(StretchClipRegion());
+        }
+
+        IEnumerator StretchClipRegion()
+        {
+            while(true)
+            {
+                Vector4 clipRange = new Vector4(panel.clipRange.x, panel.clipRange.y, uiCamera.pixelWidth + offset.x, uiCamera.pixelHeight + offset.y);
+                panel.clipRange = clipRange;
+                yield return new WaitForSeconds(updateInterval);
+            }
+
+        }
+    }
+}
---
+++ Assets/Inventory/UserInterface/UniformScale.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UserInterface
+{
+    [ExecuteInEditMode]
+    public class UniformScale : MonoBehaviour
+    {
+        public float updateInterval = 0.1f;
+        Coroutine updateCoroutine = null;
+
+        void Start()
+        {
+            if (updateCoroutine == null)
+                StartCoroutine(SetUniformScale());    
+        }
+
+        IEnumerator SetUniformScale()
+        {
+            while(true)
+            {
+                Vector3 parentScale = new Vector3(transform.lossyScale.x / transform.localScale.x,
+                                                  transform.lossyScale.y / transform.localScale.y,
+                                                  transform.lossyScale.z / transform.localScale.z);
+
+                Vector3 tempScale = new Vector3(transform.localScale.x / parentScale.x,
+                                                transform.localScale.y / parentScale.y,
+                                                transform.localScale.z / parentScale.z);
+
+                transform.localScale = tempScale;
+                yield return new WaitForSeconds(updateInterval);
+            }
+        }
+
+    }
+}
